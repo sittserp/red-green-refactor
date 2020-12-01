5371965693abb01a86e95d1460c3d7cb50d4eda8
Perry Sittser <sittserp@gmail.com>
2020-12-01 14:23:47 -0800

    get-name with dot notation

--- get-name.js
+++ get-name.js
@@ -1,6 +1,8 @@
 const spot = { name: 'spot', age: 5, weight: '20 lbs' };
 
-
+const getName = obj => {
+    return obj.name;
+}
 
 module.exports = {
     getName
--- get-name.test.js
+++ get-name.test.js
@@ -18,4 +18,4 @@ const spot = { name: 'spot', age: 5, weight: '20 lbs' }
 getName(spot) // returns spot
 
 const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
-getName(character) // returns Aang
+getName(character) // returns Aang */
