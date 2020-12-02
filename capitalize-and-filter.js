df0cf8d8b098e1cbd1b73fe2b91d6bc24a47784a
Perry Sittser <sittserp@gmail.com>
2020-12-01 16:29:17 -0800

    capitalize-and-filter with array methods .filter and .map

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,13 +1,10 @@
 const dogs = ['spot', 'rover', 'bingo', 'fred']
 
 const capitalizeAndFilter = (arr) => {
-    const newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].charAt(0) !== 'f') {
-            newArr[newArr.length] = arr[i].toUpperCase()
-        }
-    }
-    return newArr
+
+    return arr
+        .filter(item => item.charAt(0) !== 'f')
+        .map((item) => item.toUpperCase())
 }
 
 module.exports = {
